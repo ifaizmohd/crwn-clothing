0ed9a1e89beaeb1078e0e425ce7a816223149ae2
ifaizmohd <faiz.mohd778@gmail.com>
2021-09-28 21:47:17 +0530

    Converting components to styled components for cart-icon, directory and collection-preview container

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import CollectionItem from "../collection-item/collection-item.component";
-import "./collection-preview.styles.scss";
+import {
+  CollectionPreviewContainer,
+  PreviewContainer,
+  Title,
+} from "./collection-preview.styles";
 
 const CollectionPreview = ({ title, items }) => (
-  <div className="collection-preview">
-    <h1 className="title">{title}</h1>
-    <div className="preview">
+  <CollectionPreviewContainer>
+    <Title>{title}</Title>
+    <PreviewContainer>
       {items
         .filter((item, idx) => idx < 4)
         .map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-    </div>
-  </div>
+    </PreviewContainer>
+  </CollectionPreviewContainer>
 );
 
 export default CollectionPreview;
